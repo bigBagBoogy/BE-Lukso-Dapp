82a6312ab83bf7222d26a67a73a134e3061671a9
maarten <van_den_bergmaarten@hotmail.com>
2023-11-01 08:09:34 +0100

    getAssetProperties

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 5000;
 const cors = require("cors");
 const { fetchAssetData } = require("./02-fetch-asset-data.js");
+const { fetchOwnedAssets } = require("./fetch-owned-assets.js");
+const { getAssetProperties } = require("./extract-asset-data.js");
 
 app.use(cors());
 
@@ -12,6 +14,21 @@ app.get("/assets", async (req, res) => {
   try {
     // Call the fetchOwnedAssets function to retrieve assets
     const assets = await fetchAssetData(
+      "0x3F0350EaFc25Cc9185a77394B7E2440ec002e466"
+    ); // Provide the address you want to fetch assets for
+
+    // Send the assets as a JSON response
+    res.json({ assets });
+  } catch (error) {
+    console.error("Error fetching assets:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/get-assets", async (req, res) => {
+  try {
+    // Call the fetchOwnedAssets function to retrieve assets
+    const assets = await fetchOwnedAssets(
       "0x3F0350EaFc25Cc9185a77394B7E2440ec002e466"
     ); // Provide the address you want to fetch assets for
 
---
+++ extract-asset-data.js
@@ -0,0 +1,108 @@
+// extract-asset-data.js
+console.log("Extracting asset data...");
+
+// Imports
+const Web3 = require("web3");
+const { ERC725 } = require("@erc725/erc725.js");
+const LSP4Schema = require("@erc725/erc725.js/schemas/LSP4DigitalAsset.json");
+
+// Static variables
+const RPC_ENDPOINT = "https://rpc.testnet.lukso.gateway.fm";
+const IPFS_GATEWAY = "https://api.universalprofile.cloud/ipfs";
+// const SAMPLE_ASSET_ADDRESS = "0x6395b330F063F96579aA8F7b59f2584fb9b6c3a5";
+const SAMPLE_ASSET_ADDRESS = "0x3F0350EaFc25Cc9185a77394B7E2440ec002e466";
+
+// Parameters for the ERC725 instance
+const provider = new Web3.providers.HttpProvider(RPC_ENDPOINT);
+const config = { ipfsGateway: IPFS_GATEWAY };
+
+// Fetchable Asset information
+let assetImageLinks = [];
+let fullSizeAssetImage;
+let assetIconLinks = [];
+let fullSizeIconImage;
+let assetDescription;
+
+/*
+ * Get the dataset of an asset
+ *
+ * @param address of the asset
+ * @return string of the encoded data
+ */
+async function fetchAssetData(address) {
+  try {
+    const digitalAsset = new ERC725(LSP4Schema, address, provider, config);
+    return await digitalAsset.fetchData("LSP4Metadata");
+  } catch (error) {
+    console.log("Could not fetch asset data: ", error);
+  }
+}
+
+/*
+ * Read properties of an asset
+ */
+async function getAssetProperties(assetJSON) {
+  let assetImageData = [];
+  let iconImageData = [];
+  try {
+    if (assetJSON.value.LSP4Metadata.images[0]) {
+      assetImageData = assetJSON.value.LSP4Metadata.images;
+      for (let i in assetImageData) {
+        assetImageLinks.push([
+          i,
+          assetImageData[i].url.replace("ipfs://", IPFS_GATEWAY),
+        ]);
+      }
+      console.log(
+        "Asset Image Links: " +
+          JSON.stringify(assetImageLinks, undefined, 2) +
+          "\n"
+      );
+
+      fullSizeAssetImage = assetImageLinks[0][1];
+      console.log("Full Size Asset Image Link: " + fullSizeAssetImage + "\n");
+    } else {
+      console.log("Asset does not have image data \n");
+    }
+
+    if (assetJSON.value.LSP4Metadata.icon[0]) {
+      iconImageData = assetJSON.value.LSP4Metadata.icon;
+      for (let i in iconImageData) {
+        assetIconLinks.push([
+          i,
+          iconImageData[i].url.replace("ipfs://", IPFS_GATEWAY),
+        ]);
+      }
+
+      console.log(
+        "Asset Icon Links: " +
+          JSON.stringify(assetIconLinks, undefined, 2) +
+          "\n"
+      );
+
+      fullSizeIconImage = assetIconLinks[0][1];
+      console.log("Full Size Icon Image Link: " + fullSizeIconImage + "\n");
+    } else {
+      console.log("Asset does not have icon data");
+    }
+
+    if (assetJSON.value.LSP4Metadata.description) {
+      assetDescription = assetJSON.value.LSP4Metadata.description;
+      console.log("Asset Description: " + assetDescription + "\n");
+    } else {
+      console.log("Asset does not have description data \n");
+    }
+  } catch (error) {
+    console.log("Could not fetch all asset properties: ", error);
+  }
+}
+// Debug
+
+fetchAssetData(SAMPLE_ASSET_ADDRESS).then((assetData) => {
+  console.log(JSON.stringify(assetData, undefined, 2));
+  getAssetProperties(assetData);
+});
+
+module.exports = {
+  getAssetProperties,
+};
